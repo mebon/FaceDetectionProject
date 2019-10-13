90779a90978ebeb0bc258dfd1bc25bb124a40ba0
M. E. Berkay KOCAOĞLU <50452706+mebon@users.noreply.github.com>
2019-10-14 00:20:34 +0300

    Düzenleme yapıldı.

--- FaceDetection/FaceDetection.py
+++ FaceDetection/FaceDetection.py
@@ -1,7 +1,7 @@
 import cv2
 
 face_cascade = cv2.CascadeClassifier('C:/Users/bekom/AppData/Local/Programs/Python/Python37-32/Lib/site-packages/cv2/data/haarcascade_frontalface_default.xml')
-resimler = ['images/groupSelfie.jpg','images/futbol.jpg','images/manzara.jpg']
+resimler = ['images/futbol.jpg','images/groupSelfie.jpg','images/manzara.jpg']
 
 for i in resimler:
 
